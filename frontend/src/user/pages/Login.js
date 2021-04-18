58b6d554ac3a6b30e76e0db76db1b3d1283f83e4
D Marcus <mindlesscorporatedrone@gmail.com>
2021-04-17 22:17:14 -0700

    added modal to login component

--- frontend/src/user/pages/Login.js
+++ frontend/src/user/pages/Login.js
@@ -5,11 +5,13 @@ import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import useInputState from "../../hooks/useInputState";
 import { AuthContext } from '../../shared/context/auth-context';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal'
 
 
 function Login(props) {
   // const { loginUser } = props;
   const auth = useContext(AuthContext);
+  const [displayError, setDisplayError] = useState();
 
   const history = useHistory();
 
@@ -27,7 +29,7 @@ function Login(props) {
             history.push('/chirps');
           }
         })
-        .catch((err) => console.log(err.response.data.message));
+        .catch((err) => setDisplayError('Login failed, please try again.'));
     } catch (error) {
       console.log(error)
     }
@@ -36,78 +38,81 @@ function Login(props) {
 
 
   return (
-    <Formik
-      initialValues={{
-        username: "",
-        password: ""
-      }}
-      onSubmit={async values => {
-        loginUser(values.username,values.password)
-      }}
+    <div>
+      {displayError && <ErrorModal message={displayError} />}
+      <Formik
+        initialValues={{
+          username: "",
+          password: ""
+        }}
+        onSubmit={async values => {
+          loginUser(values.username, values.password)
+        }}
 
-      validationSchema={Yup.object().shape({
-        username: Yup.string()
-          .required("Required"),
-        password: Yup.string()
-          .required("Required")
-      })}
-    >
-      {props => {
-        const {
-          values,
-          touched,
-          errors,
-          dirty,
-          isSubmitting,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          handleReset
-        } = props;
-        return (
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="username" style={{ display: "block" }}>
-              Username
+        validationSchema={Yup.object().shape({
+          username: Yup.string()
+            .required("Required"),
+          password: Yup.string()
+            .required("Required")
+        })}
+      >
+        {props => {
+          const {
+            values,
+            touched,
+            errors,
+            dirty,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            handleReset
+          } = props;
+          return (
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="username" style={{ display: "block" }}>
+                Username
             </label>
-            <input
-              id="username"
-              placeholder="Enter your username"
-              type="text"
-              value={values.username}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={
-                errors.username && touched.username
-                  ? "text-input error"
-                  : "text-input"
-              }
-            />
-            {errors.username && touched.username && (
-              <div className="input-feedback">{errors.username}</div>
-            )}
+              <input
+                id="username"
+                placeholder="Enter your username"
+                type="text"
+                value={values.username}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={
+                  errors.username && touched.username
+                    ? "text-input error"
+                    : "text-input"
+                }
+              />
+              {errors.username && touched.username && (
+                <div className="input-feedback">{errors.username}</div>
+              )}
 
-            <label htmlFor="passowrd">Password</label>
-            <input
-              type="password"
-              name="password"
-              onBlur={handleBlur}
-              onChange={handleChange}
-              value={values.password}
-            />
-            {errors.password && touched.password && (
-              <span className="error">
-                {errors.password}
-              </span>
-            )}
+              <label htmlFor="passowrd">Password</label>
+              <input
+                type="password"
+                name="password"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.password}
+              />
+              {errors.password && touched.password && (
+                <span className="error">
+                  {errors.password}
+                </span>
+              )}
 
-            <button type="submit" disabled={isSubmitting}>
-              Submit
+              <button type="submit" disabled={isSubmitting}>
+                Submit
             </button>
 
-          </form>
-        );
-      }}
-    </Formik>
+            </form>
+          );
+        }}
+      </Formik>
+    </div>
 
   );
 }
