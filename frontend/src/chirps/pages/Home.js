5070a27bb401ae119375a3551483d5d3cbe22df7
D Marcus <mindlesscorporatedrone@gmail.com>
2021-05-22 20:04:52 -0700

    added fade & background image to home page

--- frontend/src/chirps/pages/Home.js
+++ frontend/src/chirps/pages/Home.js
@@ -8,18 +8,20 @@ function Home() {
     const history = useHistory();
 
     return (
-        <div className="Home">
-            <div className="Home-titles">
-                <h1>Happening Now</h1>
-                <h2>Join Chirp today.</h2>
-            </div>
+        <div className="Background">
+            <div className="Home">
+                <div className="Home-titles">
+                    <h1>Happening Now</h1>
+                    <h2>Join Chirp today.</h2>
+                </div>
 
-            <Link to="auth/register">
-                <button>Register</button>
-            </Link>
-            <Link to="auth/login">
-                <button>Login</button>
-            </Link>
+                <Link to="auth/register">
+                    <button>Register</button>
+                </Link>
+                <Link to="auth/login">
+                    <button>Login</button>
+                </Link>
+            </div>
         </div>
     )
 }
